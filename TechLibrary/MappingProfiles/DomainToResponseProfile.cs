9d7cd344103b863923e4053fa6c40ae0f0c4c257
CORP\jss9226 <jagraj.singh@avistacorp.com>
2020-02-25 11:02:12 -0800

    Changes related to the Tech Library

--- TechLibrary/MappingProfiles/DomainToResponseProfile.cs
+++ TechLibrary/MappingProfiles/DomainToResponseProfile.cs
@@ -10,7 +10,7 @@ namespace TechLibrary.MappingProfiles
         public DomainToResponseProfile()
         {
             CreateMap<Book, BookResponse>().ForMember(x => x.Descr, opt => opt.MapFrom(src => src.ShortDescr))
-                                           .ForMember(x => x.PublishedDate, opt => opt.MapFrom(src => Convert.ToDateTime(src.PublishedDate).ToString("yyyy-MM-dd")));
+                                           .ForMember(x => x.PublishedDate, opt => opt.MapFrom(src => !string.IsNullOrEmpty(src.PublishedDate) ? Convert.ToDateTime(src.PublishedDate).ToString("yyyy-MM-dd") : string.Empty));
             CreateMap<BookRequest, Book>().ForMember(x => x.ShortDescr, opt => opt.MapFrom(src => src.Descr));
             CreateMap<BookResult, BookResultResponse>().ForMember(x => x.Items, opt => opt.MapFrom(src => src.Items));
         }
